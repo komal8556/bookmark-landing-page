fd3486dee1489bbd037685b9b764e89b420e4315
komal8556 <komalmishra8586@gmail.com>
2023-10-12 16:47:40 +0530

    fix email validation

--- js/toggleMobileNavigation.js
+++ js/toggleMobileNavigation.js
@@ -2,7 +2,6 @@ const openNavBtn = document.getElementById("toggle-btn");
 const closeNavIcon = document.getElementById("toggle-btn-close")
 const mobileNavigation = document.querySelector(".navigation-mobile")
 
-// console.log(mobileNavigation);
 
 let openMobileNavigation = () => (mobileNavigation.classList.remove("hidden"))
 let closeMobileNavigation = () => (mobileNavigation.classList.add("hidden"))
